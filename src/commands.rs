6b7005f25483c082f226c07353165a2daf07dd69
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-03-20 22:39:36 -0400

    Added functions for: making new branches, adding code to git, making a commit, pushing code.

--- src/commands.rs
+++ src/commands.rs
@@ -3,38 +3,70 @@ use std::env::{set_current_dir};
 use std::process::{Command, exit};
 
 // Main git commands
-pub struct Git;
+pub struct GitCommands;
 
-impl Git {
+impl GitCommands {
     pub fn clone(website: &str, author: &str, repo: &str) {
         Command::new("git")
             .arg("clone")
             .arg(format_args!("{}{}{}", website, author, repo).to_string().as_str())
             .spawn();
-        set_current_dir(repo);
     }
 
-    pub fn checkoutb(repo: &str, branch: &str) {
+    pub fn checkout_b(repo: &str, branch: &str) {
         if is_git_repo(repo) != true {
             println!("{} is not a git repository", repo);
             exit(0);
         }
-        set_current_dir(repo);
         Command::new("git")
             .arg("checkout")
             .arg(branch)
             .spawn();
     }
 
-    pub fn checkoutt(repo: &str, tag: &str) {
+    pub fn checkout_t(repo: &str, tag: &str) {
         if is_git_repo(repo) != true {
             println!("{} is not a git repository", repo);
             exit(0);
         }
-        set_current_dir(repo);
         Command::new("git")
             .arg("checkout")
             .arg(tag)
+            .spawn();
+    }
+
+    pub fn checkout_nb(repo: &str, branch: &str) {
+        if is_git_repo(repo) != true {
+            println!("{} is not a git repository", repo);
+            exit(0);
+        }
+        Command::new("git")
+            .arg("checkout")
+            .arg("-b")
+            .arg(repo)
+            .spawn();
+    }
+
+    pub fn add(&mut self) {
+        Command::new("git")
+            .arg("add")
+            .arg(".")
+            .spawn();
+    }
+
+    pub fn commit(&mut self message: &str) {
+        Command::new("git")
+            .arg("commit")
+            .arg("-m")
+            .arg(message)
+            .spawn();
+    }
+
+    pub fn push(&mut self, message: &str) {
+        self.add();
+        self.commit(message);
+        Command::new("git")
+            .arg("push")
             .spawn();
     }
 }
